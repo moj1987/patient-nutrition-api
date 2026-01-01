82a89207589e91847a974f7366a72163b99cd3b0
moj <mojtaba.abbasszadeh@gmail.com>
2026-01-01 15:04:03 -0500

    Add validation

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,4 +1,10 @@
 class FoodItem < ApplicationRecord
   has_many :meal_food_items
   has_many :meals, through: :meal_food_items
+
+  validates :name, presence: true, uniqueness: true
+  validates :calories, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :carbs, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :protein, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :fat, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,9 @@
 class Patient < ApplicationRecord
     has_many :meals
     enum :status, { active: 0, discharged: 1 }
+
+    validates :name, presence: true
+    validates :room_number, presence: true
+    validates :status, presence: true
+    validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
